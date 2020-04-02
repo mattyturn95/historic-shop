aeeb3370c120ca3c3bd9b2a849fe1cc409a5a281
mattyturn95 <mattyturn95@gmail.com>
2020-04-02 19:58:09 +0000

    added env.py

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLISHABLE',
+    'pk_test_DtjNU4L2x6Bm8bwHHXkMAsrK00AOoNWfS1')
+os.environ.setdefault(
+    'STRIPE_SECRET', 'sk_test_5o9EHB6ymQhd43s5MgHsTBjW00hqF7gxI4')
