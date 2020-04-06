3abd811641bef76fba5e6e919eca24a3c8e86d82
mattyturn95 <mattyturn95@gmail.com>
2020-04-06 18:58:29 +0000

    changed env variables for deployment

--- env.py
+++ env.py
@@ -1,6 +1,8 @@
 import os
 
-os.environ.setdefault('STRIPE_PUBLISHABLE',
-    'pk_test_DtjNU4L2x6Bm8bwHHXkMAsrK00AOoNWfS1')
-os.environ.setdefault(
-    'STRIPE_SECRET', 'sk_test_5o9EHB6ymQhd43s5MgHsTBjW00hqF7gxI4')
+os.environ['STRIPE_PUBLISHABLE'] = 'pk_test_DtjNU4L2x6Bm8bwHHXkMAsrK00AOoNWfS1'
+os.environ['STRIPE_SECRET'] = 'sk_test_5o9EHB6ymQhd43s5MgHsTBjW00hqF7gxI4'
+os.environ['DATABASE_URL'] = 'postgres://ciifgmhacfozie:b7a65d9d825c48e97b4bc904ccd60f87cfbc26ab700950eab8b9239ed7bde133@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d8ab4ls4r1t9fv'
+os.environ['SECRET_KEY'] = '$07qw_246tg%kas9rw_t++qyepvpj!!6^127a*4$rmje!ufd*l'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIASVWSLXZVTWJPURUX'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
